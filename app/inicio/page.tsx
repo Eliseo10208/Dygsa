f4acc423c869fd5681f5679aae76de0cf77854c8
Eliseo <Eliseo10208@gmail.com>
2024-05-26 00:33:20 -0600

    se añadieron los estilos y bootstrap al proyecto

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 import { useSession } from "next-auth/react"
+import '@/app/assets/css/Styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 function Inicio(){
 
     const {data: session, status} = useSession()
@@ -8,9 +11,78 @@ function Inicio(){
     console.log(session)
 
     return (
-        <div>
-            holaa inicio
+        <div className="control-container">
+        <div className="control-nav">
+            <div className="logo"></div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-home"></i>
+                    <div className="nav-txt">Registro de viaje</div>
+                </div>
+            </div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-truck"></i>
+                    <div className="nav-txt">Unidades</div>
+                </div>
+                <ul>
+                    <li>Vehículos</li>
+                    <li>Remolques</li>
+                    <li>Configuración</li>
+                </ul>
+            </div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-id-card"></i>
+                    <div className="nav-txt">Operadores</div>
+                </div>
+            </div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-road"></i>
+                    <div className="nav-txt">Rutas</div>
+                </div>
+            </div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-users"></i>
+                    <div className="nav-txt">Clientes</div>
+                </div>
+            </div>
+            <div className="nav-group">
+                <div>
+                    <i className="fa fa-screwdriver"></i>
+                    <div className="nav-txt">Administración</div>
+                </div>
+                <ul>
+                    <li>Accesos</li>
+                    <li>Permisos</li>
+                </ul>
+            </div>
         </div>
+    
+        <div className="control-content">
+            <div className="control-header">
+                <button className="nav-bar">
+                    <i className="fa fa-bars"></i>
+                </button>
+    
+                <div className="control-user">
+                    <button className="btn">
+                        <img src="/assets/img/photodefault.png" /> Nombre de Usuario <i className="fas fa-caret-down"></i>
+                    </button>
+                    <ul>
+                        <li>Configuración</li>
+                        <li>Salir</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="content">
+                {/* Aquí se puede incluir el contenido condicional */}
+            </div>
+        </div>
+    </div>
+    
     )
 }
 export default Inicio
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,43 +1,62 @@
-"use client";
-import { FormEvent, useState } from "react";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// pages/login.tsx
+'use client'
+import { FormEvent, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import '@/app/assets/css/Styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 function LoginPage() {
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-try {
-        const signinResponse = await signIn("credentials", {
-        username: formData.get("email") as string,
-        password: formData.get("password") as string,
-        redirect: false,
-      });
-    if (signinResponse?.error) return setError(signinResponse.error as string);
-    if (signinResponse?.ok) return router.push("/inicio");
-    console.log(signinResponse);
-} catch (error) {
-    console.error(error);
-}
-    
+        const formData = new FormData(e.currentTarget);
+        try {
+            const signinResponse = await signIn("credentials", {
+                redirect: false,
+                username: formData.get("email") as string,
+                password: formData.get("password") as string,
+            });
 
-    
-  };
+            if (signinResponse?.error) {
+                setError(signinResponse.error);
+            } else if (signinResponse?.ok) {
+                router.push('/inicio');
+            } else {
+                console.log(signinResponse);
+            }
+        } catch (error) {
+            console.error(error);
+            setError("An unexpected error occurred.");
+        }
+    };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        {error && <div>{error}</div>}
-        <h1>Login</h1>
-        <input type="email" name="email" placeholder="winter@gmail.com" />
-        <input type="password" name="password" placeholder="******" />
-        <button>Iniciar Sesion</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="login-content">
+            <div className="login-logo">
+                <img src="/assets/img/logo.png" alt="Logo" />
+            </div>
+            <h6>HOLA, Bienvenido de nuevo!</h6>
+            <form data-ajax="false" className="login-form" onSubmit={handleSubmit}>
+                {error && <div className="alert alert-danger">{error}</div>}
+                <div className="login-group">
+                    <input name="email" type="email" className="login-input" placeholder="Correo Electrónico" required />
+                </div>
+                <div className="login-group">
+                    <input name="password" type="password" className="login-input" placeholder="Contraseña" required />
+                    <div style={{ textAlign: 'right' }}>
+                        <button type="button" className="btn btn_recovery">¿Olvidaste la contraseña?</button>
+                    </div>
+                </div>
+                <div className="login-button">
+                    <button type="submit" className="btn btn-login">Ingresar</button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
